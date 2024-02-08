6ab719e0f218887f9e87f49b8404ef9dd865b404
Ashutosh Pathak <ashu.p89@gmail.com>
2024-02-08 17:26:13 +0530

    the BIG flaw was i was passing jwk-set-uri of MASTER realm, instead of ashu1 realm, in appln.props file :P

--- src/main/java/com/ashu/oauth2/config/SecurityConfig.java
+++ src/main/java/com/ashu/oauth2/config/SecurityConfig.java
@@ -1,22 +1,36 @@
 package com.ashu.oauth2.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
+//@EnableWebSecurity
+@Configuration
 public class SecurityConfig {
 
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .authorizeHttpRequests((authorizeRequests) -> authorizeRequests.anyRequest().authenticated())
+//                .cors(Customizer.withDefaults())
+//                .csrf(Customizer.withDefaults())
+//                .sessionManagement(Customizer.withDefaults())
+//                .oauth2ResourceServer((oauth2ResourceServer) -> oauth2ResourceServer.jwt(Customizer.withDefaults()))
+//                .build();
+//    }
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests((authorizeRequests) -> authorizeRequests.anyRequest().authenticated())
-                .cors(Customizer.withDefaults())
-                .csrf(Customizer.withDefaults())
-                .sessionManagement(Customizer.withDefaults())
-                .oauth2ResourceServer((oauth2ResourceServer) -> oauth2ResourceServer.jwt(Customizer.withDefaults()))
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.cors(Customizer.withDefaults())
+                .csrf(CsrfConfigurer::disable)
+                .authorizeHttpRequests(httpRequests -> httpRequests.anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()))
                 .build();
     }
 }
