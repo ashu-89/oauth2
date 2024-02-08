e34893422bfc9a97195bf20bb65baf8ee08682d3
Ashutosh Pathak <ashu.p89@gmail.com>
2024-02-08 17:38:42 +0530

    very happy

--- src/main/java/com/ashu/oauth2/controller/HelloController.java
+++ src/main/java/com/ashu/oauth2/controller/HelloController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/hello")
 public class HelloController {
 
+    //In postman both works -
+    //Authorization tab -> OAuth2.0 -> configure new token for client creds -> generate new token -> use token
+    //Or
+    //Headers -> Authorization Bearer <<bearer token generated above>>
+
     @GetMapping
     public String hello(){
         return "Hello";
